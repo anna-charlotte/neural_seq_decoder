a44029dbc414ca53fffe2d08559903dddddbd16a
anna-charlotte <chgerhaher@yahoo.com>
2024-06-19 21:40:42 +0100

    store synthetic data

--- scripts/store_synthetic_data.py
+++ scripts/store_synthetic_data.py
@@ -1,62 +1,57 @@
-# for train dataloader, do inference for the input phonemes, and store the phonemes, text, and model output, similarly to what the normal data is stored as.
+import pickle
 from pathlib import Path
 
-from neural_decoder.neural_decoder_trainer import getDatasetLoaders
+from neural_decoder.neural_decoder_trainer import (
+    _padding,
+    getDataLoader,
+    getDatasetLoaders,
+)
 from text2brain.models.utils import load_text2brain_model
+from text2brain.synthetic_data import save_synthetic_data
 
 if __name__ == "__main__":
     root_dir = Path("/data/engs-pnpl/lina4471/synthetic_data_willett2023/simple_rnn")
-    data_file = Path("/data/engs-pnpl/lina4471/willett2023/competitionData/pytorchTFRecords.pkl")
+    input_data_file = Path("/data/engs-pnpl/lina4471/willett2023/competitionData/pytorchTFRecords.pkl")
 
     # load model from check point
     model_dir = root_dir / "model"
     model = load_text2brain_model(model_dir=model_dir)
-    print(f"model.__class__.__name__ = {model.__class__.__name__}")
 
     # load dataloaders
     train_dl, _, _ = getDatasetLoaders(
-        datasetName=data_file,
+        datasetName=input_data_file,
         batchSize=1,
+    )
+
+    file_name = "pytorchTFRecords_synthetic.pkl"
+    save_synthetic_data(
+        model=model,
+        dl=train_dl,
+        dir_path=root_dir / "synthetic_data",
+        file_name=file_name,
+        args={"input_data_file": input_data_file},
     )
 
     # for synthetic data generated, also store an args file with:
     # - datapath: text data that was used as input, condition
     # (- modelpath: model that translated, generated brain signals)
 
-    n_days = 24
-    data = [
-        {
-            "sentenceDat": [],
-            "phonemes": [],
-            "phoneLens": [],
-        }
-        for _ in range(n_days)
-    ]
-
-    # extract data
-    for batch in train_dl:
-
-        _, text, _, _, dayIdx = batch
-        # print(f"X.size() = {X.size()}")
-        neural_signals = model.predict(text, dayIdx)
-        # print(f"y.size() = {y.size()}")
-        data[dayIdx]["sentenceDat"].append(neural_signals)
-        data[dayIdx]["phonemes"].append(text)
-        data[dayIdx]["phoneLens"].append(len(text))
-
-    # store data
-    file = root_dir / "data" / "pytorchTFRecords_synthetic.pkl"
-    with open(file, "wb") as handle:
-        print(f"Write files to: {file}")
-        pickle.dump(data, handle)
-
+    file = root_dir / "synthetic_data" / "pytorchTFRecords_synthetic.pkl"
     # load dat again
+    print(f"\nLoad stored generated data from: {file}")
     with open(file, "rb") as handle:
         loaded_data = pickle.load(handle)
 
     train_dl = getDataLoader(
-        data=loadedData,
+        data=loaded_data,
         batch_size=16,
         shuffle=True,
-        collate_fn=None,
+        collate_fn=_padding,
     )
+    for i, batch in enumerate(train_dl):
+        print()
+        y, text, y_len, X_len, dayIdx = batch
+        print(f"text.size() = {text.size()}")
+        print(f"y.size() = {y.size()}")
+        print(f"type(text) = {type(text)}")
+        neural_signals = model.predict(text, dayIdx)
--- src/text2brain/synthetic_data.py
+++ src/text2brain/synthetic_data.py
@@ -1,2 +1,49 @@
-def store_synthetic_data():
-    pass
+import pickle
+from pathlib import Path
+
+from torch.utils.data import DataLoader
+
+from text2brain.models.model_interface import TextToBrainInterface
+
+
+def _generate_data(model: TextToBrainInterface, dl: DataLoader, n_days: int = 24) -> dict:
+    data = [
+        {
+            "sentenceDat": [],
+            "phonemes": [],
+            "phoneLens": [],
+        }
+        for _ in range(n_days)
+    ]
+
+    print(f"\nGenerate data ...")
+    for i, batch in enumerate(dl):
+        _, text, _, _, dayIdx = batch
+        neural_signals = model.predict(text, dayIdx)
+
+        data[dayIdx]["sentenceDat"].append(neural_signals[0])
+        data[dayIdx]["phonemes"].append(text[0])
+        data[dayIdx]["phoneLens"].append(len(text[0]))
+        if i == 32:
+            break
+    return data
+
+
+def save_synthetic_data(
+    model: TextToBrainInterface,
+    dl: DataLoader,
+    args: dict,
+    dir_path: Path,
+    file_name: str = "pytorchTFRecords_synthetic.pkl",
+) -> None:
+    print("In store_synthetic_data() ...")
+
+    data = _generate_data(model=model, dl=dl)
+
+    file = dir_path / file_name
+    with open(file, "wb") as handle:
+        print(f"\nStore generated data to: {file}")
+        pickle.dump(data, handle)
+
+    with open(dir_path / "args", "wb") as handle:
+        pickle.dump(args, handle)
