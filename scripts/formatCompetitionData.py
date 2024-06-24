8e069dc65d42f2fca232abeb3394cdc41b2e3d06
anna-charlotte <chgerhaher@yahoo.com>
2024-06-24 08:28:17 +0100

    add data exploration and visualization notebook

--- scripts/formatCompetitionData.py
+++ scripts/formatCompetitionData.py
@@ -2,60 +2,15 @@ import os
 import pickle
 import re
 
-import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 import scipy.io
 from g2p_en import G2p
 from tqdm import tqdm
 
+from neural_decoder.phoneme_utils import phoneToId
+
 g2p = G2p()
-PHONE_DEF = [
-    "AA",
-    "AE",
-    "AH",
-    "AO",
-    "AW",
-    "AY",
-    "B",
-    "CH",
-    "D",
-    "DH",
-    "EH",
-    "ER",
-    "EY",
-    "F",
-    "G",
-    "HH",
-    "IH",
-    "IY",
-    "JH",
-    "K",
-    "L",
-    "M",
-    "N",
-    "NG",
-    "OW",
-    "OY",
-    "P",
-    "R",
-    "S",
-    "SH",
-    "T",
-    "TH",
-    "UH",
-    "UW",
-    "V",
-    "W",
-    "Y",
-    "Z",
-    "ZH",
-]
-PHONE_DEF_SIL = PHONE_DEF + ["SIL"]
-
-
-def phoneToId(p):
-    return PHONE_DEF_SIL.index(p)
 
 
 def loadFeaturesAndNormalize(sessionPath):
---
+++ src/neural_decoder/phoneme_utils.py
@@ -0,0 +1,51 @@
+PHONE_DEF = [
+    "AA",
+    "AE",
+    "AH",
+    "AO",
+    "AW",
+    "AY",
+    "B",
+    "CH",
+    "D",
+    "DH",
+    "EH",
+    "ER",
+    "EY",
+    "F",
+    "G",
+    "HH",
+    "IH",
+    "IY",
+    "JH",
+    "K",
+    "L",
+    "M",
+    "N",
+    "NG",
+    "OW",
+    "OY",
+    "P",
+    "R",
+    "S",
+    "SH",
+    "T",
+    "TH",
+    "UH",
+    "UW",
+    "V",
+    "W",
+    "Y",
+    "Z",
+    "ZH",
+]
+PHONE_DEF_SIL = PHONE_DEF + ["SIL"]
+
+
+def phoneToId(p: str):
+    return PHONE_DEF_SIL.index(p)
+
+
+def idToPhone(idx: int):
+    assert idx in range(0, len(PHONE_DEF_SIL))
+    return PHONE_DEF_SIL[idx]
