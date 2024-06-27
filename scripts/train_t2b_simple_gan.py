222d3f2c056fd3c9bba1a823cd22da54943184a2
anna-charlotte <chgerhaher@yahoo.com>
2024-06-27 10:38:15 +0100

    extract PhonemeImageGAN class

--- scripts/train_t2b_simple_gan.py
+++ scripts/train_t2b_simple_gan.py
@@ -1,142 +1,25 @@
+import argparse
 import os
 import pickle
+import random
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Union
 
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-
-from neural_decoder.dataset import PhonemeDataset
-from neural_decoder.phoneme_utils import ROOT_DIR
-from neural_decoder.neural_decoder_trainer import get_data_loader
-from text2brain.visualization import plot_brain_signal_animation
-
-import argparse
-import os
-import random
-import torch
-import torch.nn as nn
 import torch.nn.parallel
 import torch.optim as optim
 import torch.utils.data
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
-
-class PhonemeImageGAN(nn.Module):
-    def __init__(self, latent_dim, n_classes, n_channels, ndf, ngf, device, n_critic=5, clip_value=0.01, lr=1e-4):
-        super(PhonemeImageGAN, self).__init__()
-        self.g = Generator(latent_dim, n_classes, ngf).to(device)
-        self.d = Discriminator(n_channels, n_classes, ndf).to(device)
-        self.device = device
-        self.n_critic = n_critic
-        self.clip_value = clip_value
-        self.lr = lr
-
-        self.g.apply(self._weights_init)
-        self.d.apply(self._weights_init)
-
-        self.optim_d = optim.RMSprop(self.d.parameters(), lr=self.lr)
-        self.optim_g = optim.RMSprop(self.g.parameters(), lr=self.lr)
-
-    def _weights_init(self, m):
-        if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.BatchNorm2d):
-            nn.init.normal_(m.weight.data, 0.0, 0.02)
-
-    def forward(self, data):
-        X_real, y, _, _ = data
-        y = y.to(self.device)
-        X_real = X_real.to(self.device)
-        X_real = X_real.view(X_real.size(0), X_real.size(1), 16, 16)
-        X_real = X_real.view(X_real.size(0), X_real.size(1), 16, 16)
-
-        for _ in range(self.n_critic):
-            self.d.zero_grad()
-            output_real = self.d(X_real, y)
-            noise = torch.randn(y.size(0), self.g.latent_dim, device=self.device)
-            X_fake = self.g(noise, y)
-            output_fake = self.d(X_fake.detach(), y)
-            errD = -(torch.mean(output_real) - torch.mean(output_fake))
-            errD.backward()
-            self.optim_d.step()
-
-            # Weight clipping
-            for p in self.d.parameters():
-                p.data.clamp_(-self.clip_value, self.clip_value)
-
-        # Update generator
-        self.g.zero_grad()
-        output_fake = self.d(X_fake, y)
-        errG = -torch.mean(output_fake)
-        errG.backward()
-        self.optim_g.step()
-
-        return errD, errG
-
-class Generator(nn.Module):
-    def __init__(self, latent_dim, n_classes, ngf):
-        super(Generator, self).__init__()
-        self.latent_dim = latent_dim
-        self.n_classes = n_classes
-        self.ngf = ngf
-        self.label_emb = nn.Embedding(n_classes, n_classes)
-        self.model = nn.Sequential(
-            # input is Z, going into a convolution
-            nn.ConvTranspose2d(latent_dim + n_classes, ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 8),
-            nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4),
-            nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d(ngf * 4, 32, 4, 2, 1, bias=False),  # Output 32 channels, stop upscaling here
-            nn.Tanh()
-            # Final state size. (32) x 16 x 16
-        )
-
-    def forward(self, noise, labels):
-        labels = self.label_emb(labels)
-        gen_input = torch.cat((noise, labels), -1)
-        gen_input = gen_input.view(gen_input.size(0), -1, 1, 1)
-        output = self.model(gen_input)
-        return output
-
-
-class Discriminator(nn.Module):
-    def __init__(self, n_channels, n_classes, ndf):
-        super(Discriminator, self).__init__()
-
-        self.label_emb = nn.Embedding(n_classes, n_classes)
-        self.model = nn.Sequential(
-            # input is (n_channels) x 16 x 16
-            nn.Conv2d(n_channels + n_classes, ndf, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size: (ndf) x 8 x 8
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size: (ndf*2) x 4 x 4
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 1, 0, bias=False),
-            # nn.BatchNorm2d(ndf * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size: (ndf*4) x 1 x 1
-            nn.Conv2d(ndf * 4, 1, 1, 1, 0, bias=False),
-            # nn.Sigmoid()
-            # output. 1 x 1 x 1
-        )
-    
-    def forward(self, img, labels):
-        labels = self.label_emb(labels)
-        labels = labels.view(labels.size(0), labels.size(1), 1, 1)
-        labels = labels.repeat(1, 1, img.size(2), img.size(3))
-        d_in = torch.cat((img, labels), 1)
-        output = self.model(d_in)
-        return output.view(-1)
+from neural_decoder.dataset import PhonemeDataset
+from neural_decoder.neural_decoder_trainer import get_data_loader
+from neural_decoder.phoneme_utils import ROOT_DIR
+from text2brain.models.phoneme_image_gan import PhonemeImageGAN
+from text2brain.visualization import plot_brain_signal_animation
 
 
 def main(args: dict) -> None:
@@ -148,7 +31,7 @@ def main(args: dict) -> None:
     
     device = args["device"]
     batch_size = args["batch_size"]
-    n_classes = 41
+    phoneme_cls = 4
     print(f"device = {device}")
 
     train_file = args["train_set_path"]
@@ -161,9 +44,10 @@ def main(args: dict) -> None:
         shuffle=True, 
         collate_fn=None, 
         dataset_cls=PhonemeDataset,
+        phoneme_cls=phoneme_cls if isinstance(phoneme_cls, int) else None
     )
     
-    print(f"len(train_dl) = {len(train_dl)}")
+    print(f"len(train_dl.dataset) = {len(train_dl.dataset)}")
 
     test_file = args["test_set_path"]
     with open(test_file, "rb") as handle:
@@ -175,9 +59,16 @@ def main(args: dict) -> None:
         shuffle=False, 
         collate_fn=None, 
         dataset_cls=PhonemeDataset, 
+        phoneme_cls=phoneme_cls if isinstance(phoneme_cls, int) else None
     )
-    print(f"len(test_dl) = {len(test_dl)}")
+    print(f"len(test_dl.dataset) = {len(test_dl.dataset)}")
 
+    # plot_brain_signal_animation(
+    #     signal=signal, 
+    #     save_path=ROOT_DIR / "plots"/ "data_visualization" / f"phone_{phoneme}_FAKE_ep{epoch}_i_{i}.gif" , 
+    #     title=f"Phoneme {phoneme}, Frame"
+    # )
+    
     n_channels = 32
     latent_dim = 100
     
@@ -191,7 +82,7 @@ def main(args: dict) -> None:
 
     gan = PhonemeImageGAN(
         latent_dim=latent_dim, 
-        n_classes=n_classes, 
+        phoneme_cls=phoneme_cls, 
         n_channels=n_channels, 
         ndf=ndf, 
         ngf=ngf, 
@@ -219,7 +110,7 @@ def main(args: dict) -> None:
                 signal = gan.g(noise_vector, torch.tensor([phoneme]).to(device))
                 plot_brain_signal_animation(
                     signal=signal, 
-                    save_path=ROOT_DIR / "plots"/ "data_visualization" / f"phone_{phoneme}_ep{epoch}_i_{i}.gif" , 
+                    save_path=ROOT_DIR / "plots"/ "data_visualization" / f"phone_{phoneme}_FAKE_ep{epoch}_i_{i}.gif" , 
                     title=f"Phoneme {phoneme}, Frame"
                 )
 
---
+++ src/text2brain/models/phoneme_image_gan.py
@@ -0,0 +1,173 @@
+import os
+import pickle
+from pathlib import Path
+from typing import Dict, Union
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from neural_decoder.dataset import PhonemeDataset
+from neural_decoder.phoneme_utils import ROOT_DIR
+from neural_decoder.neural_decoder_trainer import get_data_loader
+from text2brain.visualization import plot_brain_signal_animation
+
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+class PhonemeImageGAN(nn.Module):
+    def __init__(self, latent_dim: int, phoneme_cls: Union[int, list], n_channels: int, ndf: int, ngf: int, device: str, n_critic: int = 5, clip_value: float = 0.01, lr=1e-4):
+        super(PhonemeImageGAN, self).__init__()
+        if isinstance(phoneme_cls, list):
+            self.conditional=True
+        else:
+            self.conditional = False
+
+        self.g = Generator(latent_dim, phoneme_cls, ngf).to(device)
+        self.d = Discriminator(n_channels, phoneme_cls, ndf).to(device)
+        self.device = device
+        self.n_critic = n_critic
+        self.clip_value = clip_value
+        self.lr = lr
+
+        self.g.apply(self._weights_init)
+        self.d.apply(self._weights_init)
+
+        self.optim_d = optim.RMSprop(self.d.parameters(), lr=self.lr)
+        self.optim_g = optim.RMSprop(self.g.parameters(), lr=self.lr)
+
+    def _weights_init(self, m):
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.BatchNorm2d):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+    def forward(self, data):
+        X_real, y, _, _ = data
+        y = y.to(self.device)
+        X_real = X_real.to(self.device)
+        X_real = X_real.view(X_real.size(0), X_real.size(1), 16, 16)
+
+        for _ in range(self.n_critic):
+            self.d.zero_grad()
+            output_real = self.d(X_real, y)
+            noise = torch.randn(y.size(0), self.g.latent_dim, device=self.device)
+            X_fake = self.g(noise, y)
+            output_fake = self.d(X_fake.detach(), y)
+            errD = -(torch.mean(output_real) - torch.mean(output_fake))
+            errD.backward()
+            self.optim_d.step()
+
+            # Weight clipping
+            for p in self.d.parameters():
+                p.data.clamp_(-self.clip_value, self.clip_value)
+
+        # Update generator
+        self.g.zero_grad()
+        output_fake = self.d(X_fake, y)
+        errG = -torch.mean(output_fake)
+        errG.backward()
+        self.optim_g.step()
+
+        return errD, errG
+
+class Generator(nn.Module):
+    def __init__(self, latent_dim, phoneme_cls, ngf):
+        super(Generator, self).__init__()
+        self.latent_dim = latent_dim
+        self.phoneme_cls = phoneme_cls
+        self.ngf = ngf
+        if isinstance(phoneme_cls, list):
+            self.conditional = True
+            n_classes = len(phoneme_cls)
+            input_dim = latent_dim + n_classes
+            self.label_emb = nn.Embedding(n_classes, n_classes)
+        else:
+            self.conditional = False
+            input_dim = latent_dim
+            self.label_emb = None
+
+        self.model = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(input_dim, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, 32, 4, 2, 1, bias=False),  # Output 32 channels, stop upscaling here
+            nn.Tanh()
+            # Final state size. (32) x 16 x 16
+        )
+
+    def forward(self, noise, labels):
+        if self.conditional:
+            labels = self.label_emb(labels)
+            gen_input = torch.cat((noise, labels), -1)
+            gen_input = gen_input.view(gen_input.size(0), -1, 1, 1)
+        else:
+            gen_input = noise.view(noise.size(0), -1, 1, 1)
+        output = self.model(gen_input)
+        return output
+
+    def sample(self, label):
+        noise = torch.randn(1, self.g.latent_dim, device=self.device)
+        gen_img = self.g(noise, label)
+        return gen_img
+
+class Discriminator(nn.Module):
+    def __init__(self, n_channels, phoneme_cls, ndf):
+        super(Discriminator, self).__init__()
+
+        if isinstance(phoneme_cls, list):
+            self.conditional = True
+            n_classes = len(phoneme_cls)
+            input_dim = n_channels + n_classes
+            self.label_emb = nn.Embedding(n_classes, n_classes)
+        else:
+            self.conditional = False
+            input_dim = n_channels
+            self.label_emb = None
+
+        
+        self.model = nn.Sequential(
+            # input is (n_channels) x 16 x 16
+            nn.Conv2d(input_dim, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (ndf) x 8 x 8
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (ndf*2) x 4 x 4
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 1, 0, bias=False),
+            # nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (ndf*4) x 1 x 1
+            nn.Conv2d(ndf * 4, 1, 1, 1, 0, bias=False),
+            # nn.Sigmoid()
+            # output. 1 x 1 x 1
+        )
+    
+    def forward(self, img, labels):
+        if self.conditional:
+            labels = self.label_emb(labels)
+            labels = labels.view(labels.size(0), labels.size(1), 1, 1)
+            labels = labels.repeat(1, 1, img.size(2), img.size(3))
+            d_in = torch.cat((img, labels), 1)
+        else:
+            d_in = img
+        output = self.model(d_in)
+        return output.view(-1)
+
