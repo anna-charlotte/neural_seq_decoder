7013adb9741dbb93905d7efa6acd1de9267263f0
anna-charlotte <chgerhaher@yahoo.com>
2024-07-03 13:38:01 +0100

    add more tests

--- tests/neural_decoder_tests/dataset_test.py
+++ tests/neural_decoder_tests/dataset_test.py
@@ -45,3 +45,52 @@ def test_phoneme_speech_dataset():
     assert phonemes.size() == (16,)
     assert logits.size() == (16, 41)
     assert dayIdx.size() == (16,)
+
+
+def test_phoneme_speech_dataset_filter_phoneme_cls():
+    test_file = "/data/engs-pnpl/lina4471/willett2023/competitionData/rnn_train_set_with_logits.pkl"
+    with open(test_file, "rb") as handle:
+        data = pickle.load(handle)
+
+    dl = get_data_loader(
+        data=data,
+        batch_size=16,
+        shuffle=False,
+        collate_fn=None,
+        dataset_cls=PhonemeDataset,
+        phoneme_ds_filter={"phoneme_cls": 2},
+    )
+    assert len(dl.dataset) == 7237
+
+    neural_windows, phonemes, logits, dayIdx = next(iter(dl))
+
+    assert neural_windows.size() == (16, 32, 256)
+    assert phonemes.size() == (16,)
+    assert logits.size() == (16, 41)
+    assert dayIdx.size() == (16,)
+
+
+def test_phoneme_speech_dataset_filter_correctness():
+    test_file = "/data/engs-pnpl/lina4471/willett2023/competitionData/rnn_test_set_with_logits.pkl"
+    with open(test_file, "rb") as handle:
+        data = pickle.load(handle)
+
+    filter_by = {"correctness_value": "C"}
+    dl = get_data_loader(
+        data=data,
+        batch_size=16,
+        shuffle=False,
+        collate_fn=None,
+        dataset_cls=PhonemeDataset,
+        phoneme_ds_filter=filter_by,
+    )
+
+    ds = dl.dataset
+    assert len(ds) == 36079
+
+    neural_windows, phonemes, logits, dayIdx = next(iter(dl))
+
+    assert neural_windows.size() == (16, 32, 256)
+    assert phonemes.size() == (16,)
+    assert logits.size() == (16, 41)
+    assert dayIdx.size() == (16,)
--- tests/text2brain_tests/models_tests/phoneme_image_gan_test.py
+++ tests/text2brain_tests/models_tests/phoneme_image_gan_test.py
@@ -4,12 +4,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from text2brain.models.phoneme_image_gan import PhonemeImageGAN
+from text2brain.models.phoneme_image_gan import PhonemeImageGAN, phonemes_to_signal
 
 
-def test_unconditional_gan():
+def test_phonemes_to_signal():
     latent_dim = 100
-    phoneme_cls = None
+    phoneme_cls = [0, 1, 2, 3, 4]
     n_channels = 32
     ndf = 64
     ngf = 64
@@ -29,6 +29,35 @@ def test_unconditional_gan():
         clip_value=clip_value,
         lr=lr,
     )
+    phonemes = [2, 2, 4, 1, 2, 0, 2]
+    gen_signal = phonemes_to_signal(gan, phonemes)
+    assert gen_signal.size(0) == 1
+    assert gen_signal.size(1) == len(phonemes) * 32
+
+
+def test_unconditional_gan():
+    latent_dim = 100
+    phoneme_cls = 2
+    n_channels = 32
+    ndf = 64
+    ngf = 64
+    device = "cpu"
+    n_critic = 5
+    clip_value = 0.01
+    lr = 1e-4
+
+    gan = PhonemeImageGAN(
+        latent_dim=latent_dim,
+        phoneme_cls=phoneme_cls,
+        n_channels=n_channels,
+        ndf=ndf,
+        ngf=ngf,
+        device=device,
+        n_critic=n_critic,
+        clip_value=clip_value,
+        lr=lr,
+    )
+    assert gan.conditional == False
 
     batch_size = 8
     X_real = torch.randn(batch_size, n_channels, 256).view(batch_size, n_channels, 16, 16)
@@ -52,7 +81,7 @@ def test_unconditional_gan():
 
 def test_conditional_gan():
     latent_dim = 100
-    phoneme_cls = 2
+    phoneme_cls = [0, 1, 2, 3, 4]
     n_channels = 32
     ndf = 64
     ngf = 64
@@ -72,10 +101,11 @@ def test_conditional_gan():
         clip_value=clip_value,
         lr=lr,
     )
+    assert gan.conditional == True
 
     batch_size = 8
     X_real = torch.randn(batch_size, n_channels, 256).view(batch_size, n_channels, 16, 16)
-    y = torch.randint(2, phoneme_cls + 1, (batch_size,))
+    y = torch.randint(0, max(phoneme_cls) + 1, (batch_size,))
 
     gan.to(device)
     X_real = X_real.to(device)
